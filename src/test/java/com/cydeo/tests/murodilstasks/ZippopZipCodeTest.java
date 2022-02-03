fc4331d24a26c33b14175c2b6c566204040cef2f
sayidimrannazari <Sayidi_n@yahoo.com>
2022-02-03 12:21:16 -0500

    rest assured day 6

--- src/test/java/com/cydeo/tests/murodilstasks/ZippopZipCodeTest.java
+++ src/test/java/com/cydeo/tests/murodilstasks/ZippopZipCodeTest.java
@@ -5,13 +5,22 @@ import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.*;
+
 public class ZippopZipCodeTest extends ZippopTestBase {
 
     @Test
     public void zipopTest(){
 
-        Response response = given().accept(ContentType.JSON)
-                           .when().get("/22031");
-        System.out.println(response.statusCode());
+        try {
+            Response response = given().accept(ContentType.JSON)
+                    .when().get("/us/22031");
+
+        }catch (IllegalStateException e){
+            System.out.println("Exception caught");
+
+        }
+
+
+
     }
 }
